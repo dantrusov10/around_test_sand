4e39a0603b87b48c57a7faa2dbb94730dc262c93
dantrusov10 <dantrusov162@gmail.com>
2026-02-22 12:41:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // Example:
 // window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwbrfWknpKxNhMLSvadmOYImOPTPwOQVUYbZ1rdw359e4aq1srw7MRDlHEzfkLiQS_Jyg/exec";
 // SANDBOX v3
-window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwZTJNNJdlizixS7-7gfQ_6eNk24AO58WhHQdlcAKeBjoZeVuJpMsZkRPic-TL4yz4wuQ/exec";
+window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyF-riUThN_gicXEdGwRxCqnnL29M1L7gnL8TdDCsPEZYf1YtMBUYWcvqnal1fxGCtkng/exec";
 
 // Backward-compatible alias used by older pages
 window.GOOGLE_SHEETS_WEBAPP_URL = window.GS_WEBAPP_URL;
