cd0ba0c5f2b121f8fda168dd0010724417c70d08
dantrusov10 <dantrusov162@gmail.com>
2026-02-22 11:44:15 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,6 +4,7 @@
 // Put your *current* Apps Script WebApp /exec URL here after each deployment.
 // Example:
 // window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwbrfWknpKxNhMLSvadmOYImOPTPwOQVUYbZ1rdw359e4aq1srw7MRDlHEzfkLiQS_Jyg/exec";
+// SANDBOX v3
 window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwZTJNNJdlizixS7-7gfQ_6eNk24AO58WhHQdlcAKeBjoZeVuJpMsZkRPic-TL4yz4wuQ/exec";
 
 // Backward-compatible alias used by older pages
