2c4a0cedde46a48786f95d4001b34615cdbb77b5
dantrusov10 <dantrusov162@gmail.com>
2026-02-25 15:33:54 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // Put your *current* Apps Script WebApp /exec URL here after each deployment.
 
 // ✅ Актуальный URL (обновляй после каждого деплоя WebApp)
-window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyxNmMsxXrjE4XGUwvOxb8z8d1M3IIviV2M4_LrlNP-330VkFSvynsCyzMJge78qQF4Fw/exec";
+window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwtBLKDLp9Okdlm0EvcUAWb5COpdHya3YTnkJJ7Vxp7uZkuIFtFi3WFgQKnum3vPdQyeA/exec";
 
 // Backward-compatible aliases used by older pages/builds
 window.GOOGLE_SHEETS_WEBAPP_URL = window.GS_WEBAPP_URL;
